f28165bc450fee3f78815d5ddd66e2b3a7a4a536
Shardul-Arya <74579131+Shardul-Arya@users.noreply.github.com>
2021-03-01 22:40:24 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,64 @@
+var dog,sadDog,happyDog;
+var feed, addFood
+var foodObj
+var foodS = 0
+var Food
+
+function preload(){
+  sadDog=loadImage("Images/Dog.png");
+  happyDog=loadImage("Images/happy dog.png");
+}
+
+function setup() {
+  createCanvas(1000,400);
+  database = firebase.database();
+  
+  dog=createSprite(800,200,150,150);
+  dog.addImage(sadDog);
+  dog.scale=0.15;
+
+  feed = createButton('Feed The Dog');
+  feed.position(700, 95);
+  feed.mousePressed(feedDog)
+
+  addFood = createButton('Add Food');
+  addFood.position(800, 95);
+  addFood.mousePressed(addFoods)
+
+  foodObj = new Foods();
+}
+
+function draw() {
+  background(46,139,87);
+
+  drawSprites();
+
+  foodObj.display();
+
+  console.log(foodObj.foodStock)
+}
+
+//function to read food Stock
+
+
+//function to update food stock and last fed time
+function feedDog() {
+  dog.addImage(happyDog);
+
+  if (foodObj.getFoodStock()<= 0) {
+    foodObj.updateFoodStock(foodObj.getFoodStock()*0);
+  }else{
+    foodObj.updateFoodStock(foodObj.foodStock = foodObj.foodStock-1);
+  }
+}
+
+
+//function to add food in stock
+function addFoods() {
+  foodS++;
+  database.ref('/').update({
+    Food:foodS
+  })
+
+  foodObj.foodStock = foodObj.foodStock + 1
+}
